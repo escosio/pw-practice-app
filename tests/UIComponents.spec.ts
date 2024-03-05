b59e780961605eb38b1c3ebdbf36c91e8aa7470d
Scott D'Antuono <scott.dantuono@newretirement.com>
2024-03-05 12:02:23 -0500

    sliders

--- tests/UIComponents.spec.ts
+++ tests/UIComponents.spec.ts
@@ -25,3 +25,27 @@ test('datepicker', async({page}) => {
     await page.locator('[class="day-cell ng-star-inserted"]').getByText(expectedDate, {exact: true}).click()
     await expect(calendarInputField).toHaveValue(dateToAssert)
 })
+
+test('sliders', async({page}) => {
+    // update attribute
+    const tempGauge = page.locator('[tabTitle="Temperature] ngx-temperature-dragger circle')
+    await tempGauge.evaluate( node => {
+        node.setAttribute('cx', '232.630')
+        node.setAttribute('cy', '232.630')
+    })
+    await tempGauge.click()
+
+    // Mouse movement
+    const tempBox = page.locator('[tabTitle="Temperature] ngx-temperature-dragger')
+    await tempBox.scrollIntoViewIfNeeded()
+
+    const box = await tempBox.boundingBox()
+    const x = box.x + box.width / 2
+    const y = box.y + box.height / 2
+    await page.mouse.move(x, y)
+    await page.mouse.down() // clicks and holds
+    await page.mouse.move(x + 100, y)
+    await page.mouse.move(x + 100, y + 100)
+    await page.mouse.up()
+    await expect(tempBox).toContainText('30')
+})
